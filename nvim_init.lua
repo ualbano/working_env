a8f52205ca3e6928af91f9d4b633abaae4cc149c
Umberto Albano <umberto.albano@planet.de>
2025-09-09 10:50:53 +0200

    feat: Add lazygit (to neovim)
    I want to test lazygit as a good git cli "gui".
    This commit is adding this and a plugin for neovim.

--- nvim_init.lua
+++ nvim_init.lua
@@ -288,7 +288,26 @@ require("lazy").setup({
       },
     },
   },
-
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
