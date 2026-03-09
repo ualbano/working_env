8b959b38fb92fcadb565176a642e5a1de60cfa7d
Umberto Albano <umberto@albano.xyz>
2026-03-09 20:35:09 +0100

    activate rust analyser

--- nvim_init.lua
+++ nvim_init.lua
@@ -708,7 +708,7 @@ require("lazy").setup({
         marksman = {},
         yamlls = {},
         ansiblels = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
